f6a26893b3495d7d2bb4681ef5b150805bf5c104
ASaltykov <alexander.saltykov@odysseusinc.com>
2017-05-12 15:57:22 +0300

    no message

--- src/modules/SearchTerms/components/View/components/Connections/selectors.tsx
+++ src/modules/SearchTerms/components/View/components/Connections/selectors.tsx
@@ -1,17 +1,30 @@
 import { createSelector } from 'reselect';
 import { get } from 'lodash';
+import { GraphNode, GraphConnection } from './presenter';
 
-const getRawConnections = (state: Object) => get(state, 'searchTerms.relations.data', {
-    terms: [],
-    links: [],
-}) || {
-    terms: [],
-    links: [],
-};
+const getRawConnections = (state: Object) => get(state, 'searchTerms.relations.data', []) || [];
 
 const getConnections = createSelector(
   getRawConnections,
-  connections => connections
+  (connections) => {
+  	const terms = connections;
+  	const links: Array<GraphConnection> = [];
+  	terms.forEach((node: GraphNode) => {
+  		if(node.parents && node.parents.length > 1) {
+  			for(let i=1; i<node.parents.length; i++) {
+  				links.push({
+	  				source: node.parents[i],
+	  				target: node.id,
+	  			});
+  			}
+  		}
+  	});
+
+  	return {
+  		terms,
+  		links,
+  	};
+  }
   );
 
 export default {
