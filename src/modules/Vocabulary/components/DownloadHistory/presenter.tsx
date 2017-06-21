c38a28b17237471e7763aa7d4d3fdb38042cf0c8
ASaltykov <alexander.saltykov@odysseusinc.com>
2017-06-21 14:12:30 +0300

    Made download bundles expandable

--- src/modules/Vocabulary/components/DownloadHistory/presenter.tsx
+++ src/modules/Vocabulary/components/DownloadHistory/presenter.tsx
@@ -9,6 +9,10 @@ import {
 import BEMHelper from 'services/BemHelper';
 import { paths, bundleStatuses } from 'modules/Vocabulary/const';
 import * as moment from 'moment';
+import {
+  Accordion,
+  AccordionItem,
+} from 'react-sanfona';
 
 require('./style.scss');
 
@@ -49,82 +53,100 @@ interface IDownloadHistoryProps extends IDownloadHistoryStateProps, IDownloadHis
   removeBundle: (id: number) => any;
 };
 
-function BundleName({ name, date }) {
+interface IDownloadHistoryStatefulProps {
+  expand: (id: number) => any;
+  expandedBundleId: number;
+};
+
+function BundleName({ name, date, onClick, isOpened }) {
   const dateFormat = 'h:mm A | MM/DD/YYYY';
   const classes = BEMHelper('bundle-caption');
 
-  return <div {...classes()}>
-    {name}
-    <span {...classes('date')}>{moment(date).format(dateFormat)}</span>
+  return <div {...classes()} onClick={onClick}>
+    <span {...classes({ element: 'opener', modifiers: { opened: isOpened } })}>keyboard_arrow_right</span>
+    <div {...classes('title-wrapper')}>
+      {name}
+      <span {...classes('date')}>{moment(date).format(dateFormat)}</span>
+     </div>
   </div>;
 }
 
-function VocabsList(props: IDownloadHistoryProps) {
-  const { isLoading, history, removeBundle } = props;
+function BundleTitle({ bundle, removeBundle, expand, isExpanded }) {
+  const classes = BEMHelper('download-history');
+
+  return <Toolbar
+    caption={<BundleName {...bundle} onClick={() => expand(bundle.id)} isOpened={isExpanded} />}
+  >
+    {[bundleStatuses.READY].includes(bundle.status)
+      ? <div>
+          <Button
+            {...classes('download-button')}
+            link={bundle.link}
+            mods={['rounded']}
+          >
+            Download
+          </Button>
+          <Button
+            {...classes('remove-button')}
+            onClick={() => removeBundle(bundle.id)}
+          >
+            Remove
+        </Button>
+      </div>
+     : <span {...classes('status')}>{bundle.status}</span>
+    }
+  </Toolbar>;
+}
+
+function VocabsList(props: IDownloadHistoryProps & IDownloadHistoryStatefulProps) {
+  const { isLoading, history, removeBundle, expand, expandedBundleId } = props;
   const classes = BEMHelper('download-history');
 
   return (    
     <div {...classes()}>
       <Toolbar caption='Download history' backUrl={paths.vocabsList()} />
-      <div>
-        {history && history.map((bundle: IDownloadRequest, index: number) => [
-            <div
+        <Accordion activeItems={[expandedBundleId]}>
+          {history && history.map((bundle: IDownloadRequest, index: number) =>
+            <AccordionItem
+              title={<BundleTitle
+                bundle={bundle}
+                removeBundle={removeBundle}
+                expand={expand}
+                isExpanded={bundle.id === expandedBundleId}
+               />}
               {...classes('bundle-caption')}
               key={`caption${index}`}
+              slug={bundle.id}
             >
-              <Toolbar
-                caption={<BundleName {...bundle} />}
-              >
-                {[bundleStatuses.READY].includes(bundle.status)
-                  ? <div>
-                    <Button
-                      {...classes('download-button')}
-                      link={bundle.link}
-                      mods={['rounded']}
-                    >
-                      Download
-                    </Button>
-                    <Button
-                      {...classes('remove-button')}
-                      onClick={() => removeBundle(bundle.id)}
-                    >
-                      Remove
-                  </Button>
-                  </div>
-                 : <span {...classes('status')}>{bundle.status}</span>
-                }
-              </Toolbar>
-             </div>,
-            <Table
-              {...classes('table')}
-              data={bundle.vocabularies}
-              mods={['hover', 'padded', 'selectable']}
-              key={`table${index}`}
-             >
-                <TableCellText
-                  {...classes('id')}
-                  header='ID'
-                  field='id'
-                />
-                <TableCellText
-                  {...classes('cdm')}
-                  header='CDM'
-                  field='cdmVersion'
-                />
-                <TableCellText
-                  {...classes('code')}
-                  header='Code (cdm v5)'
-                  field='code'
-                />
-                <TableCellText
-                  {...classes('name')}
-                  header='Name'
-                  field='name'
-                />
-            </Table>
-          ]
+              <Table
+                {...classes('table')}
+                data={bundle.vocabularies}
+                mods={['hover', 'padded', 'selectable']}
+               >
+                  <TableCellText
+                    {...classes('id')}
+                    header='ID'
+                    field='id'
+                  />
+                  <TableCellText
+                    {...classes('cdm')}
+                    header='CDM'
+                    field='cdmVersion'
+                  />
+                  <TableCellText
+                    {...classes('code')}
+                    header='Code (cdm v5)'
+                    field='code'
+                  />
+                  <TableCellText
+                    {...classes('name')}
+                    header='Name'
+                    field='name'
+                  />
+              </Table>          
+            </AccordionItem>
         )}
-      </div>
+      </Accordion>
       <LoadingPanel active={isLoading} />
     </div>
   );
