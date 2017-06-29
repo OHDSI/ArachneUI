89127000646c914ccedab724bcc17b686776c09c
Maria Pozhidaeva <maria.pozhidaeva@odysseusinc.com>
2017-06-29 17:26:28 +0300

    ATHENA-128 - Logout does not actually logout

--- src/modules/Auth/reducers/index.ts
+++ src/modules/Auth/reducers/index.ts
@@ -4,6 +4,7 @@ import services from '../apiServices';
 
 export default combineReducers({
 	core,
+	logout: services.auth_logout.reducer,
 	principal: services.auth_principal.reducer,
 	professionalTypes: services.auth_professionalTypes.reducer,
 });
