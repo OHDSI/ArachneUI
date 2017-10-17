a211325bea881c36aa37f595ea07bb8f60a2678a
agackovka <anton.gackovka@odysseusinc.com>
2017-10-17 14:45:27 +0300

    Fixed different colors for NOT_COLLECTED

--- src/modules/AnalysisExecution/components/ViewEditAnalysis/ModalSubmitCode/selectors.js
+++ src/modules/AnalysisExecution/components/ViewEditAnalysis/ModalSubmitCode/selectors.js
@@ -22,6 +22,7 @@
 
 import { createSelector } from 'reselect';
 import get from 'lodash/get';
+import { healthStatuses } from 'const/dataSource';
 
 const getRawStudySourceList = state => get(state, 'analysisExecution.studyDataSourceList.queryResult.result') || [];
 
@@ -31,8 +32,8 @@ const getDataSourceOptions = createSelector(
     value: dataSource.id,
     label: `${get(dataSource, 'dataNode.name', '')}: ${dataSource.name}`,
     healthStatus: {
-      title: dataSource.healthStatusTitle,
-      value: dataSource.healthStatus,
+      title: healthStatuses.getTitle(dataSource.healthStatus),
+      value: healthStatuses.getColor(dataSource.healthStatus)
     },
   }))
 );
