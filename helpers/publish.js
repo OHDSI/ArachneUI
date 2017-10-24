fd0ec2e5172bbb4407cbcd9cdee9354b7fd6bbc7
ASaltykov <alexander.saltykov@odysseusinc.com>
2017-10-24 17:53:30 +0300

    fixed name

--- helpers/publish.js
+++ helpers/publish.js
@@ -1,7 +1,8 @@
 var cmd = require('node-command-line');
 var version = require('../package.json').version;
+var name = require('../package.json').name;
 
-var result = cmd.run('npm info arachne-ui-components@' + version);
+var result = cmd.run('npm info ' + name + '@' + version);
 result.then((r) => {
   if (!r.message.length) {
     // version is not published yet
