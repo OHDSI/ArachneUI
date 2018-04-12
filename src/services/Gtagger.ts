4ed17eee6b49c9873755ab644ea4e5486a583179
ASaltykov <alexander.saltykov@odysseusinc.com>
2018-04-12 18:10:32 +0300

    no message

--- src/services/Gtagger.ts
+++ src/services/Gtagger.ts
@@ -3,7 +3,7 @@ import { Gtagger } from 'gtagger';
 const trackingId = 'UA-116831829-1';
 
 export function StartAnalytics() {
-  if (!__DEV__) {
+  if (__DEV__) {
     return false;
   }
   Gtagger.initialize(trackingId);
