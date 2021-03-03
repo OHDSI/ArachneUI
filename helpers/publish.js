a2c16bc527719425cf973d7070f01305e5308680
alex cumarav <tsumarev@gmail.com>
2021-03-03 10:13:19 +0100

    loglevel

--- helpers/publish.js
+++ helpers/publish.js
@@ -6,7 +6,7 @@ var result = cmd.run('npm info ' + name + '@' + version);
 result.then((r) => {
   if (!r.message.length) {
     // version is not published yet
-    cmd.run('npm publish');
+    cmd.run('npm --loglevel verbose publish');
   }else{
     console.error(`Skip publish because version ${name}.${version} is already published`);
     process.exit(1);
