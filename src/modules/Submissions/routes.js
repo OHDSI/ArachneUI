e3fed69503cb956cdd27fc80fd5b354e5cb2f343
Sergei Sukhanov <sergei.n.sukhanov@gmail.com>
2023-10-18 12:48:29 +0200

    fix route

--- src/modules/Submissions/routes.js
+++ src/modules/Submissions/routes.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import { Route } from 'react-router';
 import ListDataSources from './components/List/index';
-import Test from './components/List/FileExplorer/FileExplorer';
+import FileBrowser from './components/List/FileExplorer/FileExplorer';
 
 function Routes() {
   return [
     <Route path="list" component={ListDataSources} />,
     <Route
       path=":submissionId/results"
-      component={Test}
+      component={FileBrowser}
     />
   ];
 }
